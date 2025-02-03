bcde09be4f5d3c61b40b315b80f686dd8ac195c0
Zohaib <zohaib.anwar0310@gmail.com>
2025-02-03 09:07:18 +0500

    Sending static html files from server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+//Setup Static Folders(like .html)
+// app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send({ message: "Hellow World" });
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(8000, () => console.log(`Server is listening at port 8000`));
